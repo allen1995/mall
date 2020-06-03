48c5486b8c5e5351ca9ed34595c9711f01468a82
allen <872062426@qq.com>
2020-06-03 23:46:29 +0800

    feat:springbootfreemarker 1.springboot集成freemarker模板

---
+++ springbootbase/src/main/resources/templates/booksbyfreemarker.ftl
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <head>
+        <meta charset="UTF-8">
+        <title>图书列表</title>
+    </head>
+    <body>
+        <tr>
+            <td>图书编号</td>
+            <td>图书名称</td>
+            <td>图书作者</td>
+        </tr>
+        <#if books ??&&(books?size>0)>
+        <tr></tr>
+    </body>
+</head>
+</html>
---
+++ springbootfreemarker/src/main/java/com/allen/springbootfreemarker/BookController.java
@@ -0,0 +1,33 @@
+package com.allen.springbootfreemarker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Auther: allen
+ * @Date: 2020-05-31 16:29
+ * @Description:
+ */
+@Controller
+public class BookController {
+
+
+    @GetMapping("/books")
+    public ModelAndView getBooksByFreeMarker(){
+        ModelAndView modelAndView = new ModelAndView();
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(1,"罗贯中","三国演义"));
+        books.add(new Book(2,"施耐庵", " 水浒传"));
+        books.add(new Book(3,"曹雪芹","红楼梦"));
+
+        modelAndView.addObject("books", books);
+        modelAndView.setViewName("books");
+
+        return modelAndView;
+    }
+
+}
