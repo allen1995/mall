7fa834691d277a6d661086f54235b35514283620
王伦 <872062426@qq.com>
2021-05-31 17:27:28 +0800

    feat:搭建mall项目基础框架

---
+++ malldemo/src/test/java/com/allen/malldemo/MalldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allen.malldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalldemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
